212efe8295f5e50b9744d56c1b7e363460c8639b
eranbabayov <114342254+eranbabayov@users.noreply.github.com>
2024-12-13 12:10:59 +0200

    Update producer.py

--- producer.py
+++ producer.py
@@ -6,3 +6,4 @@ if __name__ == '__main__':
     msg = Message(sender_id=person2.name, recipient_id="eran1", message=f"hello from the other side message number")
     person2.broker.out_channel.basic_publish(exchange="", routing_key="eran1", body=msg.msg_to_bytes(),
                                              properties=pika.BasicProperties(delivery_mode=2))
+    print("message sent")
